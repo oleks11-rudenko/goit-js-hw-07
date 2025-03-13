8aedd28bb0ebb4eb96c5d6c3c92282ebbcc86c51
oleks11-rudenko <oleksii-rudenko@ukr.net>
2025-03-13 19:25:30 +0200

    Resolved task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,9 @@
 const numberOfCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${numberOfCategories.length}`);
 
-const tempList = [];
 const elements = document.querySelectorAll(".item ul");
-const numberOfElements = elements.forEach((element) =>
-  tempList.push(element.innerText.split("\n"))
-);
-
 const categories = document.querySelectorAll("h2");
-const result = categories.forEach((elem, index) => {
-  console.log(`Category: ${elem.textContent}`);
-  console.log(`Elements: ${tempList[index].length}`);
+const numberOfElements = elements.forEach((elem, index) => {
+  console.log(`Category: ${categories[index].textContent}`);
+  console.log(`Elements: ${elem.innerText.split("\n").length}`);
 });
