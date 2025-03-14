a0a3fc0433ed28cfefe20473250c1498359cba76
oleks11-rudenko <oleksii-rudenko@ukr.net>
2025-03-14 16:26:23 +0200

    Solved task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const textColor = document.querySelector(".color");
+const changeColor = document.querySelector(".change-color");
+
+changeColor.addEventListener("click", changeBodyAndTextColor);
+
+function changeBodyAndTextColor() {
+  const randColor = getRandomHexColor();
+  document.body.style.backgroundColor = randColor;
+  textColor.textContent = randColor;
+}
+
+// style="background-color: ;
