b4d93b111afb4833e0bb01723372c71acc32030d
oleks11-rudenko <oleksii-rudenko@ukr.net>
2025-03-14 14:25:51 +0200

    Solved task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", submitLogin);
+
+function submitLogin(event) {
+  event.preventDefault();
+
+  if (
+    !event.target.elements.email.value ||
+    !event.target.elements.password.value
+  ) {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: event.target.elements.email.value.trim(),
+    password: event.target.elements.password.value.trim(),
+  };
+
+  console.log(formData);
+
+  event.target.reset();
+}
